c3fea6328b90fe4fedcfe45608015c9fc3ffb65b
Ethan Seither <ethan.seither@gmail.com>
2015-10-07 13:18:26 -0700

    Add Rule Parsing Function, Define SystemVar class
    Deffine a fucntion which takes a rule string which can include plaintext strings,
    NonterminalSymbols, and system variables which is processed, returns them in a typed array
    Also create SystemVar class to allow for user to see in real time all of the system Variables they
    have defined within the context free grammar.

--- grammar.py
+++ grammar.py
@@ -1,5 +1,10 @@
-import random
 
+"""
+Module designed to be used for the generation of generative grammars that focuses entirely on
+generating grammars as opposed to parsing them
+"""
+import random
+import re
 
 class NonterminalSymbol(object):
     """A non-terminal symbol in a grammar."""
@@ -16,6 +21,13 @@ class NonterminalSymbol(object):
         self.rules.append(rule_object)
         self._fit_probability_distribution()
 
+    def remove_rule(self, derivation, application_rate):
+        """remove a production rule for this nonterminal symbol."""
+        rule_object = Rule(symbol=self, derivation=derivation, application_rate=application_rate)
+        if rule_object in self.rules:
+            self.rules.remove(rule_object)
+            self._fit_probability_distribution()
+
     def expand(self):
         """Expand this nonterminal symbol by probabilistically choosing a production rule."""
         selected_rule = self._pick_a_production_rule()
@@ -25,10 +37,17 @@ class NonterminalSymbol(object):
         """Probabilistically select a production rule."""
         x = random.random()
         selected_rule = next(
-            rule for rule in self.rules_probability_distribution if
-            self.rules_probability_distribution[rule][0] < x < self.rules_probability_distribution[rule][1]
-        )
+                rule for rule in self.rules_probability_distribution if (
+                self.rules_probability_distribution[rule][0] < x <
+                self.rules_probability_distribution[rule][1])
+                )
         return selected_rule
+
+    def __str__(self):
+        return '[['+self.tag.__str__()+']]'
+
+    def __repr__(self):
+        return self.__str__()
 
     def _fit_probability_distribution(self):
         """Return a probability distribution fitted to the given application-rates dictionary."""
@@ -49,8 +68,8 @@ class NonterminalSymbol(object):
         # Make sure the last bound indeed extends to 1.0
         last_bound_attributed = list(probabilities)[-1]
         fitted_probability_distribution[last_bound_attributed] = (
-            fitted_probability_distribution[last_bound_attributed][0], 1.0
-        )
+                fitted_probability_distribution[last_bound_attributed][0], 1.0
+                )
         self.rules_probability_distribution = fitted_probability_distribution
 
 
@@ -65,18 +84,107 @@ class TerminalSymbol(object):
         """Return this terminal symbol."""
         return self.representation
 
+    def __str__(self):
+        return "\"" + self.representation.__str__() + "\""
+
+    def __repr__(self):
+        return self.__str__()
+
+class SystemVar(TerminalSymbol):
+    """
+    class to handle systemVariables which are processed by game engine
+    instead of Expressionist, seperate from TerminalSymbol so that
+    we can provide a list of all systemVar easily
+    """
+
+    def __init__(self, representation):
+        TerminalSymbol.__init__(self, representation)
+
+    def __str__(self):
+        return "\[" + self.representation + "\]"
+
+    def __repr__(self):
+        return self.__str__
+
 
 class Rule(object):
     """A production rule in a grammar."""
 
-    def __init__(self, symbol, derivation, application_rate):
+    def __init__(self, symbol, derivation, application_rate=1):
         """Initialize a Rule object."""
-        self.symbol = symbol
+        self.symbol = symbol # NonterminalSymbol that is lhs of rule
         self.derivation = derivation  # An ordered list of nonterminal and terminal symbols
+        self.application_rate = application_rate
+
+    def __eq__(self, other):
+        return self.symbol == other.symbol and self.derivation == other.derivation and self.application_rate == other.application_rate
+
+    def modify_application_rate(self, application_rate):
+        """
+        change the application rate for a given Rule
+        """
         self.application_rate = application_rate
 
     def derive(self):
         """Carry out the derivation specified for this rule."""
         return ''.join(symbol.expand() for symbol in self.derivation)
+
+    def __str__(self):
+        return self.symbol.__str__()+ ' -> ' + self.derivation.__str__()
+
+    def __repr__(self):
+        return self.__str__()
+
+def parse_rule(rule_string):
+    """
+    function parses a string and returns the generation represented by that string
+    :param rule_string: the string representing the rule to be parsed
+    :type rule_string: string
+    :returns: list[symbols]
+    """
+
+    #this regex is a pain but it matches strings of either [[...]] or [...]
+    split_list = re.split('(\[\[[^\[{2}]+\]\]|\[[^\]]+])', rule_string)
+    #remove all empty strings
+    split_list = filter(None, split_list)
+    #
+    rhs = []
+    for token in split_list:
+        brackets = token.count("[")
+        stripped = token.lstrip("[").rstrip("]")
+
+        if brackets == 2:
+            rhs.append(NonterminalSymbol(stripped))
+        elif brackets == 1:
+            rhs.append(SystemVar(stripped))
+        else:
+            rhs.append(TerminalSymbol(stripped))
+    return rhs
+
+class PCFG(object):
+    """
+    Driver class for our PCFG, allows us to index our nonterminals and
+    system variables so that the user can more easily modify them in real time
+    Also allows us to selectively expand NonterminalSymbols to see all of their productions
+    """
+
+    def __init__(self):
+        self.nonterminals = []
+
+    def add_nonterminal(self, nonterminal):
+        """ add a nonterminal to our grammar"""
+        if nonterminal not in self.nonterminals:
+            self.nonterminals.append(nonterminal)
+
+
+
+
+
+
+
+
+
+
+
 
 
