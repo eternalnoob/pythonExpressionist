aac53fec040f195bea8454ce9cff8c7fe94a72da
Ethan Seither <ethan.seither@gmail.com>
2016-04-24 22:31:16 -0700

    expressionista

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI=os.environ['DATABASE_URL']
 
 class ProductionConfig(Config):
-    DEBUG = False
+    DEBUG = True
 
 class StagingConfig(Config):
     DEVELOPMENT=True
